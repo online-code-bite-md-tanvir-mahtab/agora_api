5187111ab0870ec32ded5e6dffa5122658ee4062
online-code-bite-md-tanvir-mahtab <tmahtab57@gmail.com>
2024-10-07 07:18:41 +0600

    this is going to be crazzy

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import os
 from flask import Flask,  request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 from agora_token_builder import RtcTokenBuilder
 import time
+import os
 
 app = Flask(__name__)
 
@@ -13,10 +13,10 @@ APP_ID = '3f8b6c06334e48ebae82b96e849a62ab'
 APP_CERTIFICATE = 'ae2545b63db7412b8dabe0578a388a6c'
 
 # Configure the SQLAlchemy part of the app
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') # Change to your preferred DB
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv("DATABASE_URL","sqlite:///users.db") # Change to your preferred DB
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# 'sqlite:///users.db'
+
 # Initialize the database
 db = SQLAlchemy(app)
 
